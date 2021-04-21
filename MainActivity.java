aa3003a065f1d5f98529997186ef64c8cc39ab6a
Pratik716 <43125004+PSK716@users.noreply.github.com>
2021-04-21 13:04:31 +0530

    java

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizzy.Attempt_Quiz_Section;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
